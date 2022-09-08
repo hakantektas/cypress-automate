d289288e230425efbc6c269babc051c90a1c1728
hakantektas <hakan.tektas@mobven.com>
2022-09-08 17:21:13 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'ebj6h1',
   e2e: {
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 60000,
+    pageLoadTimeout:90000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
