f23f5d6e173a817da82f1a1195100bf30faa1a50
hakantektas <hakan.tektas@mobven.com>
2022-09-08 16:28:17 +0300

    tst1

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,23 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'ebj6h1',
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1000,
+  viewportHeight: 600,
+  // Viewport settings overridden for component tests
+  component: {
+    viewportWidth: 500,
+    viewportHeight: 500
+  },
   e2e: {
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    env: {
+      baseUrl: 'http://localhost:3030',
+      FOO: 'bar'
+    }
   },
 });
