9379ba4327fbee6f789359cb644cfc8eb5b515ae
hakantektas <hakan.tektas@mobven.com>
2022-09-08 17:13:43 +0300

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 
 describe('empty spec', () => {
   it('Login with demo site', () => {
-    cy.visit("https://www.tatilbudur.com/",{ responseTimeout: 31000 })
+    cy.visit("https://www.nextflix.com/",{ responseTimeout: 61000 })
     cy.get('#key').type("Antalya")
     cy.xpath("#hotelSearchCheckIn").click()
     cy.xpath("(//*[@class='datepicker-switch'])[1]").click()
