e64042eb7dea1476d5ee4bb73c83689aa9106a5c
hakantektas <hakan.tektas@mobven.com>
2022-09-08 16:18:42 +0300

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,10 +2,11 @@
 
 describe('empty spec', () => {
   it('Login with demo site', () => {
-    cy.visit('https://www.lmax.com/global/demo-login')
-    cy.get('#username').type("test")
+    cy.visit('https://www.tatilbudur.com/')
+    cy.get('#key').type("Antalya")
     cy.get('#password').type('1234')
-    cy.xpath("(//*[contains(text(),'Login')])[6]").click()
+    cy.xpath("#hotelSearchCheckIn").click()
+    cy.xpath("(//*[@class='datepicker-switch'])[1]").click()
     
   })
 })
