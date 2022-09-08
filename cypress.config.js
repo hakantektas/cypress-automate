a7d7a915686f8c211f039e3fe999aec8227fb4ee
hakantektas <hakan.tektas@mobven.com>
2022-09-08 16:51:47 +0300

    TEST

--- cypress.config.js
+++ cypress.config.js
@@ -2,23 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'ebj6h1',
-  defaultCommandTimeout: 5000,
-  viewportWidth: 1000,
-  viewportHeight: 600,
-  // Viewport settings overridden for component tests
-  component: {
-    viewportWidth: 500,
-    viewportHeight: 500
-  },
   e2e: {
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
     },
-    env: {
-      baseUrl: 'https://www.tatilbudur.com',
-      FOO: 'bar'
-    }
   },
 });
